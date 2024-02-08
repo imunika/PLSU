28a365144f4ba65cb1340c7310d819594fe8e869
Ellie Novoa <ellie.novoa@gmail.com>
2024-02-08 16:11:49 -0700

    Changes in padding and text size

--- app/components/Biography.jsx
+++ app/components/Biography.jsx
@@ -34,14 +34,14 @@ export default function Card() {
   return (
     <>
       {uniqueRecords.map((item, key) => (
-        <div className="flex justify-center p-6" key={key}>
+        <div className="flex justify-center p-6 md:p-2" key={key}>
           <Link href={item.contributor} className="hover:no-underline">
             <img
               src={item.author_headshot}
               alt=""
               className="mx-auto rounded-full w-36 h-36 transition-all duration-300 cursor-pointer filter grayscale hover:grayscale-0"
             />
-            <h2 className="p-2 text-center text-slate-700 font-light font-taviraj text-xl hover:text-[#be3a3b]">
+            <h2 className="p-2 text-center text-slate-700 font-light font-taviraj text-xl md:text-lg hover:text-[#be3a3b]">
               {item.author_name}
             </h2>
           </Link>
